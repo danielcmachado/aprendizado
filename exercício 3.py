7d756ff559c25f2c2c6a143065feb9f2c2a80afc
daniel <danmachado997@gmail.com>
2023-02-08 11:08:30 -0300

    exercicio 3

---
+++ exercício 3.py
@@ -0,0 +1,19 @@
+print('exercicio 3 números')
+
+print('contagem de galinhas:')
+print('daniel', 25 + 30 / 6)
+print('joao', 100-25 * 3 % 4)
+
+print('contagem de ovos:')
+print(3+2+1-5+4%2-1/4+6)
+
+print('e verdade que 3+2<5-7?')
+print(3+2<5-7)
+
+print('quanto e 3+2?', 3+2)
+print('quanto é 5-7?', 5-7)
+
+print('somas:')
+print('isso é maior?', 5>-2)
+print('isso é maior ou igual?', 5>=-2)
+print('isso é menor ou igual?', 5<=-2)
