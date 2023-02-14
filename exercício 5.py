53582ad1f601d485adf4486bb2531df2a5e91979
daniel <danmachado997@gmail.com>
2023-02-14 13:54:49 -0300

    exercicio 5

---
+++ exercício 5.py
@@ -0,0 +1,16 @@
+print('exercício 5')
+
+meu_nome = 'zed a. shaw'
+minha_idade = '35'
+minha_altura = '74' 
+meu_peso = '180' 
+meus_olhos = 'castanhos'
+meu_cabelo = 'marrom'
+meus_dentes = 'brancos'
+total = 35 + 74 + 180
+print(f'vamos falar sobre {meu_nome}')
+print(f'isso é {minha_altura} um tamanho medio')
+print(f'isso e {meu_peso} um pouco pesado')
+print(f'eu gosto do {meus_olhos} dos meus olhos e do {meu_cabelo} do meu cabelo')
+print(f'essa e a cor dos meus dentes {meus_dentes} excerto quando bebo cafe')
+print(f'se eu adicionar {minha_idade}, {meu_peso}, e {minha_altura} eu tenho {total}')
