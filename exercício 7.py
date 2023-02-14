c6b062954a0b3d786a57b2ae77b22c49fb9ef022
daniel <danmachado997@gmail.com>
2023-02-14 14:15:05 -0300

    exercicio 7

---
+++ exercício 7.py
@@ -0,0 +1,22 @@
+print('exercício 7')
+
+print('maria tinha um cordeirinho')
+print('seu pelo era branco como {}.' .format('neve'))
+print('e em todos os lugares que maria ia.')
+print('.' *10) #o que ela fazia?
+
+end1 = 'c'
+end2 = 'h'
+end3 = 'e'
+end4 = 'e'
+end5 = 's'
+end6 = 'e'
+end7 = 'b'
+end8 = 'u'
+end9 = 'r'
+end10 = 'g'
+end11 = 'e'
+end12 = 'r'
+
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7+end8+end9+end10+end11+end12)
