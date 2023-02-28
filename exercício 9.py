77c65bb41f2a9565b0b19f118f3e9a085bb04f27
daniel <danmachado997@gmail.com>
2023-02-28 13:13:54 -0300

    exercício 9

---
+++ exercício 9.py
@@ -0,0 +1,16 @@
+print('exercício 9')
+
+#aqui temos uma coisa nova e estranha, lembre-se de digita-la exatamente.
+
+dias = 'segunda terça quarta quinta sexta sabado domingo'
+meses = 'jan\nfev\nmar\nabr\nmai\njun\njul\nagost'
+
+print('aqui estão os dias:', dias)
+print('aqui estão os meses:', meses)
+
+print("""
+tem alguma coisa acontecendo aqui.
+com tres aspas duplas.
+podemos digitar o tanto que quisermos.
+ate em 4 linhas se precisarmos, ou mesmo 5, ou 6.
+""")
