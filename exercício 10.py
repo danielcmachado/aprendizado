052b76c9364a407d15cd078b721652fdfbac4ea2
daniel <danmachado997@gmail.com>
2023-02-28 13:29:49 -0300

    exercício 10

---
+++ exercício 10.py
@@ -0,0 +1,29 @@
+print('exercício 10')
+gato_malhado = '\testou tabulando'
+gato_persa = 'estou dividindo \na linha no meio'
+gato_preto = 'eu sou \\ um \\ gato'
+
+gato_gordo = """
+eu vou fazer uma lista:
+\t* comida de gato
+\t* fishers
+\t* erva de gato\n\t* grama
+"""
+
+print(gato_malhado)
+print(gato_persa)
+print(gato_preto)
+print(gato_gordo)
+
+print('sequencias de escape')
+print('barra \\ invertida')
+print('\'aspas simples\'')
+print('\"aspas duplas\"')
+print('\a campainha ascii (bell)')
+print('\b retorno ascii (BS)')
+print('\favanço de página ascii (ff)')
+print('\nnova linha ascii (lf)')
+print('\rretorno de carro (cs)')
+print('\ttabulação horizontal (TAB)')
+print('\vtabulação vertical ascii (VT)')
+print('\ooo caractere com valor octal ooo')
