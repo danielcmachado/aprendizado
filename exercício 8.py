f6aac710ff420abc42fba7e8d7b88f49dd1e03e4
daniel <danmachado997@gmail.com>
2023-02-28 13:08:20 -0300

    exercício 8

---
+++ exercício 8.py
@@ -0,0 +1,14 @@
+print('exercício 8')
+
+formatter = '{} {} {} {}'
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format('um', 'dois', 'tres', 'quatro'))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format( 
+"colacando o proprio", 
+"texto aqui", 
+"teste"
+"de daniel"
+))
